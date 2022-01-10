d0648c4ba1502ad974c5ef661ad3d2ab25d4f939
Peter Muessig <peter.muessig@sap.com>
2022-01-10 10:44:50 +0100

    fix: renamed controller, remove unused imports

--- generators/app/templates/src/controller/Main.controller.ts
+++ generators/app/templates/src/controller/Main.controller.ts
@@ -1,11 +1,10 @@
 import MessageBox from "sap/m/MessageBox";
 import Controller from "sap/ui/core/mvc/Controller";
-import AppComponent from "../Component";
 
 /**
  * @namespace <%= appId %>.controller
  */
-export default class MainController extends Controller {
+export default class Main extends Controller {
 
 	public sayHello() : void {
 		MessageBox.show("Hello World!");
