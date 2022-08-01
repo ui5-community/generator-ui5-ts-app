82f7adf5b421da2d7c3b61ffc08d5cc0a844cdc4
Marian Zeis <13335743+marianfoo@users.noreply.github.com>
2022-08-01 17:48:08 +0200

    feat: add formatter and models (#11)
    * feat: add formatter and models
    
    * add formatter to Main Controller

--- generators/app/templates/src/controller/Main.controller.ts
+++ generators/app/templates/src/controller/Main.controller.ts
@@ -1,10 +1,12 @@
 import MessageBox from "sap/m/MessageBox";
 import BaseController from "./BaseController";
+import formatter from "../model/formatter";
 
 /**
  * @namespace <%= appId %>.controller
  */
 export default class Main extends BaseController {
+	private formatter = formatter;
 
 	public sayHello() : void {
 		MessageBox.show("Hello World!");
