b9f1fc00e16f38d9cda2e7f91b52f78be3aa0689
Matthias Osswald <mat.osswald@sap.com>
2024-10-23 09:09:38 +0200

    feat: Use Test Starter
    This change introduces the test starter as a replacement for the
    traditional test suite + test page setup.
    
    The feature is only available for application projects starting with
    UI5 1.124, so the existing test setup is kept for older projects.

--- generators/app/templates/webapp/test/testsuite.qunit.ts
+++ generators/app/templates/webapp/test/testsuite.qunit.ts
@@ -1,9 +1,33 @@
-/* eslint-disable */
-// @ts-nocheck
-window.suite = function () {
-	const suite = new parent.jsUnitTestSuite();
-	const sContextPath = location.pathname.match(/(.*\/)(?:[^/]+)/)?.[1];
-	suite.addTestPage(sContextPath + "unit/unitTests.qunit.html");
-	suite.addTestPage(sContextPath + "integration/opaTests.qunit.html");
-	return suite;
+export default {
+	name: "QUnit test suite for the UI5 Application: <%= namespace %>",
+	defaults: {
+		page: "ui5://test-resources/<%= appURI %>/Test.qunit.html?testsuite={suite}&test={name}",
+		qunit: {
+			version: 2
+		},
+		sinon: {
+			version: 1
+		},
+		ui5: {
+			language: "EN",
+			theme: "<%= defaultTheme %>"
+		},
+		coverage: {
+			only: "<%= appURI %>/",
+			never: "test-resources/<%= appURI %>/"
+		},
+		loader: {
+			paths: {
+				"<%= appURI %>": "../"
+			}
+		}
+	},
+	tests: {
+		"unit/unitTests": {
+			title: "Unit tests for <%= namespace %>"
+		},
+		"integration/opaTests": {
+			title: "Integration tests for <%= namespace %>"
+		}
+	}
 };
